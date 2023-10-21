dac24327b5c53ae6629eff94d7dc42c1ac4f646b
rubenjr0 <rubenjr0@proton.me>
2023-10-21 18:31:33 +0000

    ✨ better tempfiles

--- app/fingerprinting.py
+++ app/fingerprinting.py
@@ -1,6 +1,6 @@
 """This module is used to fingerprint images."""
 import json
-from os import makedirs
+import tempfile
 
 import numpy as np
 import seahash
@@ -18,8 +18,8 @@ def _build_grid(image: np.ndarray, grid_factor: int):
     # Create a list to hold the image cells
     cells = [
         image[
-            y * cell_height : (y + 1) * cell_height,
-            x * cell_width : (x + 1) * cell_width,
+            y * cell_height: (y + 1) * cell_height,
+            x * cell_width: (x + 1) * cell_width,
         ]
         for y in range(grid_factor)
         for x in range(grid_factor)
@@ -34,9 +34,10 @@ def _build_fingerprint(metadata):
 
 
 def _embed_fingerprint(idx, cell, fingerprint):
-    tempfile = f"tmp/cells/{idx}.png"
-    Image.fromarray(cell).save(tempfile)
-    fingerprinted = lsb.hide(tempfile, str(fingerprint), auto_convert_rgb=True)
+    _, temp_cell = tempfile.mkstemp('.png', f'cell_{idx}')
+    Image.fromarray(cell).save(temp_cell)
+    fingerprinted = lsb.hide(temp_cell, str(
+        fingerprint), auto_convert_rgb=True)
     fingerprinted = np.array(fingerprinted)
     return fingerprinted
 
@@ -48,8 +49,8 @@ def _merge_cells(cells, width, height, grid_factor):
             cell = next(cells)
             cell_width, cell_height, _ = cell.shape
             canvas[
-                x * cell_width : (x + 1) * cell_width,
-                y * cell_height : (y + 1) * cell_height,
+                x * cell_width: (x + 1) * cell_width,
+                y * cell_height: (y + 1) * cell_height,
             ] = cell
     return canvas
 
@@ -74,13 +75,12 @@ def fingerprint_image(image: np.ndarray, metadata: dict[str, any], grid_factor: 
         grid_factor (int, optional): How many subgrids should be added to the image. Defaults to 4.
 
     Returns:
-        _type_: The fingerprinted image, the fingerprint, and the metadata
+        (np.ndarray, int, metadata): The fingerprinted image, the fingerprint, and the metadata
     """
     cells, new_width, new_height = _build_grid(image, grid_factor)
     metadata, fingerprint = _build_fingerprint(metadata)
-    makedirs("tmp/cells")
-    makedirs("tmp/fingerprinted")
-    cells = map(lambda x: _embed_fingerprint(x[0], x[1], fingerprint), enumerate(cells))
+    cells = map(lambda x: _embed_fingerprint(
+        x[0], x[1], fingerprint), enumerate(cells))
     fingerprinted = _merge_cells(cells, new_width, new_height, grid_factor)
     return fingerprinted, fingerprint, metadata
 
@@ -97,14 +97,13 @@ def extract(image: np.ndarray):
     Returns:
         int: Fingerprint
     """
-    makedirs("tmp/reveal")
     for grid_factor in range(2, 128):
         cells, _, _ = _build_grid(image, grid_factor)
         for idx, cell in enumerate(cells):
-            tempfile = f"tmp/reveal/{idx}.png"
-            Image.fromarray(cell).save(tempfile)
+            temp_reveal = tempfile.mkstemp('.png', f'cell_{idx}')
+            Image.fromarray(cell).save(temp_reveal)
             try:
-                return int(lsb.reveal(tempfile))
+                return int(lsb.reveal(temp_reveal))
 
             except Exception:
                 continue
